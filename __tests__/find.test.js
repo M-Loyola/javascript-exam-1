c33e71d0621e8a44a715cf68c84373385803effb
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-03 17:13:12 +0800

    (JAVASCRIPT-EXAM1): commit changes for find.test

--- __tests__/find.test.js
+++ __tests__/find.test.js
@@ -1,5 +1,21 @@
 import { firstGrownUp, firstOrange, firstLengthOver5Name } from "../src/find";
 
 describe("array find test", () => {
-  // Please add test cases here
+  test("find: should return first grown up over 17", () => {
+    const ages = [11, 23, 51, 60, 12];
+    const result = firstGrownUp(ages);
+    expect(result).toBe(23);
+  });
+
+  test("find: should return first orange", () => {
+    const fruits = ["apple", "grape", "orange"];
+    const result = firstOrange(fruits);
+    expect(result).toBe("orange")
+  });
+
+  test("find: should return first name over 5 length", () => {
+    const names = ["Mich","Raymond","Gae","Sayra"];
+    const result = firstLengthOver5Name(names);
+    expect(result).toBe("Raymond")
+  });
 });
