d351d5df835ffab363576ba288ac78d8b4ce0608
M-Loyola <m.yakit.loyola@gmail.com>
2023-08-03 17:15:56 +0800

    (JAVASCRIPT-EXAM1): commit changes for filter.test

--- __tests__/filter.test.js
+++ __tests__/filter.test.js
@@ -1,5 +1,21 @@
 import { filterEvenNumbers, filterLengthWith4, filterStartWithA } from '../src/filter';
 
 describe('array filter test', () => {
-    // Please add test cases here
+    test('filter: should return array of even numbers', () => {
+        const numbers = [1,2,3,4,5,6,7,8,9,10];
+        const result = filterEvenNumbers(numbers);
+        expect(result).toEqual([2,4,6,8,10]);
+    });
+
+    test('filter: should return array with words length with 4', () => {
+        const words = ['car','bike','plane','boat','ship'];
+        const result = filterLengthWith4(words);
+        expect(result).toEqual(['bike','boat','ship']);
+    });
+
+    test('filter: should return array with words starting with a', () => {
+        const words = ['almond','cherry','apricot'];
+        const result = filterStartWithA(words);
+        expect(result).toEqual(['almond','apricot']);
+    });
 });
